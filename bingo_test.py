fb85995c62ce811136c96250e028330adbddf589
Adam Kaplan <adam.kaplan@redhat.com>
2021-10-15 00:03:30 -0400

    Pylint fixes
    - Fix arguments passed to open.
    - Fix variable substitution error.
    - Ignore no-value-for-parameter check in main.
    - Add docstrings to main code and tests.
    - Use sys.exit
    - Fix line length and indents

--- bingo_test.py
+++ bingo_test.py
@@ -1,11 +1,16 @@
+"""Unit tests for bingo"""
+
+from urllib.parse import urlencode
 import unittest
 from bingo import Bingo
-from urllib.parse import urlencode
 
 class TestBingo(unittest.TestCase):
+    """Unit tests for the Bingo class"""
 
     def test_to_url(self):
-        test_case = Bingo(title='hello', free_square='free', exclamation='yay!', terms=['hello', 'world'])
+        """Tests for Bingo.to_url()"""
+        test_case = Bingo(title='hello', free_square='free', exclamation='yay!',
+                          terms=['hello', 'world'])
         expected = 'https://www.buzzwordbingogame.com/cards/custom/?' + urlencode({
             'title': 'hello',
             'free_square': 'free',
